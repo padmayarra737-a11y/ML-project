05cbe707ca9b61039677912ddac0f2b809a4eb9e
padmayarra737-a11y <padmayarra737@gmail.com>
2026-07-20 15:31:23 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -37,8 +37,8 @@ class ModelTrainer:
             X_test, y_test = test_array[:, :-1], test_array[:, -1]
 
             models = {
-                "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest": RandomForestRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "K-Neighbors Regressor": KNeighborsRegressor(),
@@ -47,7 +47,49 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
 
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            # params={
+            #     "Decision Tree": {
+            #         "criterion": ["squared_error", "friedman_mse", "absolute_error",'poisson'],
+            #         "splitter": ["best", "random"],
+            #         "max_features": ["sqrt", "log2", None]
+            #     },
+            #     "Random Forest": {
+            #         "n_estimators": [8, 16, 32, 64, 128, 256],
+            #         "criterion": ["squared_error", "friedman_mse", "absolute_error"],
+            #         "max_depth": [5, 10, 15, 20, 30],
+            #         "max_features": ["sqrt", "log2", None]
+            #     },
+            #     "Gradient Boosting": {
+            #         "loss": ["squared_error", "huber", "absolute_error"],
+            #         "learning_rate": [0.1, 0.01, 0.05,.001],
+            #         "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+            #         "n_estimators": [8, 16, 32, 64, 128, 256]
+            #     }
+            #     ,
+            #     "Linear Regression": {},
+            #     "K-Neighbors Regressor": {
+            #         "n_neighbors": [5,7,9,11],
+            #         "weights": ["uniform", "distance"],
+            #         "algorithm": ["ball_tree", "kd_tree", "brute"],
+            #     },
+            #     "XGBoost Regressor": {
+            #         "learning_rate": [0.1, 0.01, 0.05,.001],
+            #         "n_estimators": [8, 16, 32, 64, 128, 256]
+            #     },
+            #     "CatBoosting Regressor": {
+            #         "depth": [6, 8, 10],
+            #         "learning_rate": [0.01, 0.05, 0.1],
+            #         "iterations": [30, 50, 100]
+            #     },
+            #     "AdaBoost Regressor": {
+            #         "learning_rate": [0.1, 0.01, 0.5, 0.001],
+            #         "n_estimators": [8, 16, 32, 64, 128, 256]
+            #     }
+            # }
+            
+
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train,
+                                                  X_test=X_test, y_test=y_test, models=models)
 
             # To get the best model score from the dictionary
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,12 @@ import os
 import sys
 import pandas as pd
 import numpy as np
-import dill
-from sklearn.metrics import r2_score
 
+from sklearn.metrics import r2_score
+# from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
+
+import dill
 
 def save_object(file_path,obj):
     try:
@@ -14,7 +16,7 @@ def save_object(file_path,obj):
 
         with open(file_path,'wb') as file_obj:
             dill.dump(obj,file_obj)
-            
+
     except Exception as e:
         raise CustomException(e,sys)
     
@@ -25,6 +27,19 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 
         for i in range(len(models)):
             model=list(models.values())[i]
+            # param=param[  list(models.keys())[i]]
+
+            model_name = list(models.keys())[i]
+
+            # if model_name not in param:
+            #       continue
+            # param = param[model_name]
+
+
+            # gs=GridSearchCV(model,param,cv=3)
+            # gs.fit(X_train,y_train)
+
+            # model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred=model.predict(X_train)
